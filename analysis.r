d8b49de440d0df6fb9849818dba178da1a8bc32b
Morgan Powers <morgan.c.powers@gmail.com>
2025-11-04 10:51:29 -0600

    categorized data

--- analysis.r
+++ analysis.r
@@ -4,3 +4,13 @@ library(coop)
 
 #extract Taskmaster data from csv into dataframe
 data <- read.csv("taskmaster25.csv")
+
+numerics <- data[c("Prize","Subjective","Objective","Fastest","Live",
+                   "Team","Highest","Lowest","Age","GregHarshnessRating",
+                   "Seat","Height","Children","Books","Helped",
+                   "RefusedHelp")]
+#Excluded due to trivial correlation: Winner,PPT,Place
+#Almost numeric: Siblings
+#String: BirthOrder,SecondLanguage,OutfitColor,Diet,FacialHair,HairColour,ChineseYear
+#Boolish: Language,ComedyAward,Headwear,Handedness,Gender
+print(cor(numerics,data["Place"],use="complete.obs"))#could also use a numeric conversion of Winner
