48ecbfac77886e3202185a3bb7ad74262ffed792
Morgan Powers <morgan.c.powers@gmail.com>
2025-11-05 17:34:11 -0600

    filtering added

--- analysis.r
+++ analysis.r
@@ -3,6 +3,9 @@ if(!require(coop))install.packages("coop")
 library(coop)
 source("data.r")
 
-#TODO: filter insignificant values?, put in a scatterplot
-correlation <- (cor(year,data["Place"],use="pairwise.complete.obs"))
-print(correlation[order(abs(correlation[,"Place"])),,drop=FALSE])
+#TODO: put in a scatterplot
+correlation <- (cor(all,data["Place"],use="pairwise.complete.obs"))
+sorted <- correlation[rev(order(abs(correlation[,"Place"]))),,drop=FALSE]
+filtered <- correlation[which(abs(correlation)>0.2),,drop=FALSE]
+sorted_and_filtered <- sorted[which(abs(sorted)>0.2),,drop=FALSE]
+print(sorted_and_filtered)
